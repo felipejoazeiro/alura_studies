5e312b923a80836d5180bece9612fec0578a6d1f
Felipe Joazeiro <felipe.joazeiro@ictcondor.com.br>
2023-10-04 09:04:39 -0300

    padrão de cor e padrão de texto criado.

---
+++ alubank/lib/components/color_dot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  const ColorDot({Key? key, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
