bf8b0d58b2f011738c2351a2b0e80787aa4500cf
Felipe Joazeiro <felipe.joazeiro@ictcondor.com.br>
2023-10-11 08:38:13 -0300

    Final dos testes

---
+++ Flutter/cliente_api/test/widget_test.dart
@@ -0,0 +1,22 @@
+import 'package:client_control/components/hamburger_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Testing the menu', () {
+    testWidgets('Hamburguer Menu should have "Menu"', (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Menu'), findsOneWidget);
+    });
+
+    testWidgets('Hamburguer Menu should have "Gerenciar clientes"',
+        (tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: HamburgerMenu(),
+      ));
+      expect(find.text('Gerenciar clientes'), findsOneWidget);
+    });
+  });
+}
